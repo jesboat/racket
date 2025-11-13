f10c51393d7d452e81057bc8f50fefbcf66473df
Jade Sailor <jgitpublic.45358@sai9.net>
2025-11-13 12:13:04 -0500

    [expander] cross-phase persistent: allow more quote, quote-syntax
    Previously, `quote` at the module level (i.e. on the rhs of a `define-values`
    and not wrapped in a lambda, i.e. would run at module instantiation time) was
    limited to a small subset of atomic data, and `quote-syntax` was forbidden to
    appear anywhere in the module.
    
    Loosen these restrictions, significantly. Specifically:
    
    Say that simple data are: numbers, booleans, symbols, chars, keywords, null,
    immutable strings, immutable bytes, immutable vectors with simple data for
    values, immutable hasheqs with simple data for keys and values, and immutable
    boxes with simple data for content. (These correspond roughly to things which
    the standard reader can read. It excludes: extflonums, because their predicate
    isn't in `#%kernel`; non-hasheq hashes, because hasheqs were previously allowed
    as values in xpp modules but other hashes weren't; prefab structs, because I
    was lazy.)
    
    The restrictions for `quote` are then relaxed so that any simple data can be
    quoted at the module level. (As before, arbitrary data can be quoted when
    guarded by a lambda.)
    
    For `quote-syntax`, the restriction to simple data is enforced throughout the
    module. This is necessary because the stx's quoted by a quote-syntax are, in
    essence, lifted to the module level.

--- racket/src/cs/schemified/expander.scm
+++ racket/src/cs/schemified/expander.scm
@@ -87204,12 +87204,13 @@
                                 (void)
                                 (check-body-no-disallowed-expr_0
                                  (parsed-let_-values-body e_0)))
-                              (if (let ((or-part_0 (parsed-quote-syntax? e_0)))
-                                    (if or-part_0
-                                      or-part_0
-                                      (|parsed-#%variable-reference?| e_0)))
+                              (if (|parsed-#%variable-reference?| e_0)
                                 (disallow e_0)
-                                (void)))))))))))))))
+                                (if (parsed-quote-syntax? e_0)
+                                  (check-quoted-syntax
+                                   (parsed-quote-syntax-datum e_0)
+                                   e_0)
+                                  (void))))))))))))))))
       (check-body-no-disallowed-expr_0
        (|#%name|
         check-body-no-disallowed-expr
@@ -87233,24 +87234,171 @@
 (define check-count
   (lambda (is-num_0 expected-num_0 enclosing_0)
     (if (= is-num_0 expected-num_0) (void) (disallow enclosing_0))))
+(define walk-for-quotable
+  (lambda (d_0 recur_0)
+    (let ((or-part_0 (number? d_0)))
+      (if or-part_0
+        or-part_0
+        (let ((or-part_1 (boolean? d_0)))
+          (if or-part_1
+            or-part_1
+            (let ((or-part_2 (symbol? d_0)))
+              (if or-part_2
+                or-part_2
+                (let ((or-part_3 (if (string? d_0) (immutable? d_0) #f)))
+                  (if or-part_3
+                    or-part_3
+                    (let ((or-part_4 (if (bytes? d_0) (immutable? d_0) #f)))
+                      (if or-part_4
+                        or-part_4
+                        (let ((or-part_5 (char? d_0)))
+                          (if or-part_5
+                            or-part_5
+                            (let ((or-part_6 (keyword? d_0)))
+                              (if or-part_6
+                                or-part_6
+                                (let ((or-part_7 (null? d_0)))
+                                  (if or-part_7
+                                    or-part_7
+                                    (let ((or-part_8 (regexp? d_0)))
+                                      (if or-part_8
+                                        or-part_8
+                                        (let ((or-part_9
+                                               (if (pair? d_0)
+                                                 (if (|#%app|
+                                                      recur_0
+                                                      (car d_0))
+                                                   (|#%app| recur_0 (cdr d_0))
+                                                   #f)
+                                                 #f)))
+                                          (if or-part_9
+                                            or-part_9
+                                            (let ((or-part_10
+                                                   (if (vector? d_0)
+                                                     (if (immutable? d_0)
+                                                       (call-with-values
+                                                        (lambda ()
+                                                          (values
+                                                           d_0
+                                                           (unsafe-vector-length
+                                                            d_0)))
+                                                        (lambda (vec_0 len_0)
+                                                          (letrec*
+                                                           ((for-loop_0
+                                                             (|#%name|
+                                                              for-loop
+                                                              (lambda (result_0
+                                                                       pos_0)
+                                                                (if (unsafe-fx<
+                                                                     pos_0
+                                                                     len_0)
+                                                                  (let ((v_0
+                                                                         (unsafe-vector-ref
+                                                                          vec_0
+                                                                          pos_0)))
+                                                                    (let ((result_1
+                                                                           (let ((result_1
+                                                                                  (|#%app|
+                                                                                   recur_0
+                                                                                   v_0)))
+                                                                             (values
+                                                                              result_1))))
+                                                                      (if (if (not
+                                                                               (let ((x_0
+                                                                                      (list
+                                                                                       v_0)))
+                                                                                 (not
+                                                                                  result_1)))
+                                                                            #t
+                                                                            #f)
+                                                                        (for-loop_0
+                                                                         result_1
+                                                                         (unsafe-fx+
+                                                                          1
+                                                                          pos_0))
+                                                                        result_1)))
+                                                                  result_0)))))
+                                                           (for-loop_0 #t 0))))
+                                                       #f)
+                                                     #f)))
+                                              (if or-part_10
+                                                or-part_10
+                                                (let ((or-part_11
+                                                       (if (hash? d_0)
+                                                         (if (hash-eq? d_0)
+                                                           (if (immutable? d_0)
+                                                             (letrec*
+                                                              ((for-loop_0
+                                                                (|#%name|
+                                                                 for-loop
+                                                                 (lambda (result_0
+                                                                          i_0)
+                                                                   (if i_0
+                                                                     (call-with-values
+                                                                      (lambda ()
+                                                                        (hash-iterate-key+value
+                                                                         d_0
+                                                                         i_0))
+                                                                      (lambda (k_0
+                                                                               v_0)
+                                                                        (let ((result_1
+                                                                               (let ((result_1
+                                                                                      (if (|#%app|
+                                                                                           recur_0
+                                                                                           k_0)
+                                                                                        (|#%app|
+                                                                                         recur_0
+                                                                                         v_0)
+                                                                                        #f)))
+                                                                                 (values
+                                                                                  result_1))))
+                                                                          (if (if (not
+                                                                                   (let ((x_0
+                                                                                          (list
+                                                                                           k_0
+                                                                                           v_0)))
+                                                                                     (not
+                                                                                      result_1)))
+                                                                                #t
+                                                                                #f)
+                                                                            (for-loop_0
+                                                                             result_1
+                                                                             (hash-iterate-next
+                                                                              d_0
+                                                                              i_0))
+                                                                            result_1))))
+                                                                     result_0)))))
+                                                              (for-loop_0
+                                                               #t
+                                                               (hash-iterate-first
+                                                                d_0)))
+                                                             #f)
+                                                           #f)
+                                                         #f)))
+                                                  (if or-part_11
+                                                    or-part_11
+                                                    (if (box? d_0)
+                                                      (if (immutable? d_0)
+                                                        (|#%app|
+                                                         recur_0
+                                                         (unbox d_0))
+                                                        #f)
+                                                      #f)))))))))))))))))))))))))))
 (define check-datum
   (lambda (d_0 e_0)
-    (if (let ((or-part_0 (number? d_0)))
-          (if or-part_0
-            or-part_0
-            (let ((or-part_1 (boolean? d_0)))
-              (if or-part_1
-                or-part_1
-                (let ((or-part_2 (symbol? d_0)))
-                  (if or-part_2
-                    or-part_2
-                    (let ((or-part_3 (string? d_0)))
-                      (if or-part_3
-                        or-part_3
-                        (let ((or-part_4 (bytes? d_0)))
-                          (if or-part_4 or-part_4 (null? d_0)))))))))))
-      (void)
-      (disallow e_0))))
+    (let ((or-part_0
+           (walk-for-quotable d_0 (lambda (d2_0) (check-datum d2_0 e_0)))))
+      (if or-part_0 or-part_0 (disallow e_0)))))
+(define check-quoted-syntax
+  (lambda (s/d_0 e_0)
+    (let ((or-part_0
+           (walk-for-quotable
+            (if (syntax?$1 s/d_0) (syntax-e$1 s/d_0) s/d_0)
+            (lambda (s/d2_0)
+              (check-quoted-syntax
+               s/d2_0
+               (if (syntax?$1 s/d2_0) s/d2_0 e_0))))))
+      (if or-part_0 or-part_0 (disallow e_0)))))
 (define quoted-string?
   (lambda (e_0)
     (if (parsed-quote? e_0) (string? (parsed-quote-datum e_0)) #f)))
