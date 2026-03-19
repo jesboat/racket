98cbf15b94be12e9e949a9f49d055c52c602d256
Jade Sailor <jgitpublic.45358@sai9.net>
2026-03-19 16:23:59 -0400

    an *actually* minimal define

--- racket/collects/racket/private/define-et-al.rkt
+++ racket/collects/racket/private/define-et-al.rkt
@@ -7,6 +7,7 @@
                          "cond.rkt"))
 
   (#%provide -define -define-syntax
+             ;define define-syntax define-for-syntax
              when unless
              call/ec let/ec)
 
@@ -31,31 +32,72 @@
   ; non-keyword define* forms
   ;
 
-  
-  ;; No error checking here, because these macros merely help
-  ;;  us write macros before the real define and define-syntax
-  (define-syntaxes (-define -define-syntax)
-    (let ([here (quote-syntax here)])
-      (let ([mk-define
-	     (lambda (base)
-	       (lambda (code)
-		 (let ([body (stx-cdr code)])
-		   (let ([first (stx-car body)])
-		     (cond
-		      [(identifier? first)
-		       (datum->syntax
-			here
-			`(,base (,first) ,@(stx->list (stx-cdr body)))
-			code)]
-		      [else
-		       (let ([pbody (stx-cdr body)])
-			 (datum->syntax
-			  (quote-syntax here)
-			  `(,base (,(stx-car first)) 
-				  (lambda ,(stx-cdr first) ,@(stx->list pbody)))
-			  code))])))))])
-	(values (mk-define (quote-syntax define-values))
-		(mk-define (quote-syntax define-syntaxes))))))
+
+  ; define, define-syntax, and define-values-for-syntax
+  ;
+  ; These are not directly user-visible, so we put minimal effort into error
+  ; reporting.
+  (begin-for-syntax
+    (define-values (process-function-define)
+      (lambda (full-stx prototype-stx body-list)
+        (define-values (id-or-nested-prototype) (stx-car prototype-stx))
+        (define-values (arg-spec) (stx-cdr prototype-stx))
+        (define-values (this-layer-rhs) (datum->syntax #f
+                                                       (list* (quote-syntax lambda)
+                                                              arg-spec
+                                                              body-list)
+                                                       full-stx))
+        (if (identifier? id-or-nested-prototype)
+            (values id-or-nested-prototype this-layer-rhs)
+            (if (stx-pair? id-or-nested-prototype)
+                (process-function-define full-stx id-or-nested-prototype (list this-layer-rhs))
+                (raise-syntax-error #f "bad syntax" full-stx id-or-nested-prototype)))))
+
+    (define-values (process-identifier-define)
+      (lambda (full-stx id body-list)
+        (raise-syntax-error-if (null? body-list)
+                               "bad syntax (missing expression after identifier)"
+                               full-stx)
+        (raise-syntax-error-if (pair? (cdr body-list))
+                               "bad syntax (multiple expressions after identifier)"
+                               full-stx)
+        (values id (car body-list))))
+
+    (define-values (process-define)
+      (lambda (head-for-output-form stx)
+        (define-values (lst) (syntax->list stx))
+        (raise-syntax-error-unless lst "bad syntax" stx)
+        (raise-syntax-error-unless (pair? (cdr lst)) "bad syntax" stx)
+        (define-values (id rhs)
+          (if (identifier? (cadr lst))
+              (process-identifier-define stx (cadr lst) (cddr lst))
+              (if (stx-pair? (cadr lst))
+                  (process-function-define stx (cadr lst) (cddr lst))
+                  (raise-syntax-error #f "bad syntax" stx (cadr lst)))))
+        (datum->syntax #f
+                       (list head-for-output-form (list id) rhs)
+                       stx))))
+
+  (define-syntaxes (define)
+    (lambda (stx)
+      (process-define (quote-syntax define-values) stx)))
+
+  (define-syntaxes (define-syntax)
+    (lambda (stx)
+      (process-define (quote-syntax define-syntaxes) stx)))
+
+  (define-syntaxes (define-for-syntax)
+    (lambda (stx)
+      (datum->syntax #f
+                     (list (quote-syntax begin-for-syntax)
+                           (process-define (quote-syntax define-values) stx))
+                     stx)))
+
+  (define-syntaxes (-define)
+    (make-rename-transformer (quote-syntax define)))
+
+  (define-syntaxes (-define-syntax)
+    (make-rename-transformer (quote-syntax define-syntax)))
 
   ; --------------------------------------------------
   ;
